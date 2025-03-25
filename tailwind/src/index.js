32537fa9d2031384d51d5d95556d95fb9fe4da02
PRAVEEN <praveen270304@gmail.com>
2025-03-25 13:10:39 +0530

    Add files via upload

---
+++ tailwind/src/index.js
@@ -0,0 +1,25 @@
+$("#toggle-btn").click(function(){
+       $('#toggle-nav').slideToggle("slow")
+      });
+
+
+//    $('#cat1').click(function(){
+//     $('#catlist1').slideToggle()
+//    })   
+
+//    $('#cat2').click(function(){
+//     $('#catlist2').slideToggle()
+//    })  
+
+//    $('#cat3').click(function(){
+//     $('#catlist3').slideToggle()
+//    })  
+//    $('#cat4').click(function(){
+//     $('#catlist4').slideToggle()
+//    })  
+//    $('#cat5').click(function(){
+//     $('#catlist5').slideToggle()
+//    })  
+//    $('#cat6').click(function(){
+//     $('#catlist6').slideToggle()
+//    })  
